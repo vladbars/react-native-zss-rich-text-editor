d54adcd9498fcd055fd3cf99de2f4cc765d87fcd
vladbars <vladbars@gmail.com>
2018-07-14 19:08:25 +0300

    hide title

--- src/RichTextEditor.js
+++ src/RichTextEditor.js
@@ -89,8 +89,8 @@ export default class RichTextEditor extends Component {
     const {top = 0, bottom = 0} = this.props.contentInset;
     const {marginTop = 0, marginBottom = 0} = this.props.style;
     const spacing = marginTop + marginBottom + top + bottom;
-
-    const editorAvailableHeight = -1; //Dimensions.get('window').height - keyboardHeight - spacing;
+    const editorAvailableHeight = Dimensions.get('window').height - (keyboardHeight*2) - spacing;
+   // const editorAvailableHeight = Dimensions.get('window').height - keyboardHeight - spacing;
     this.setEditorHeight(editorAvailableHeight);
   }
 
